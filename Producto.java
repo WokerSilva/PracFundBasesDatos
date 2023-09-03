80ac509cdb1916b98498dae52fdbb71d47d7ec99
wokersilva <marcosilva@ciencias.unam.mx>
2023-09-03 14:40:36 -0600

    Añadiendo getters and setters

--- Producto.java
+++ Producto.java
@@ -5,5 +5,36 @@ public class Producto{
     private double precio;
     private int cantidadEnExistencia;
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public int getCantidadEnExistencia() {
+        return cantidadEnExistencia;
+    }
+    public void setCantidadEnExistencia(int cantidadEnExistencia) {
+        this.cantidadEnExistencia = cantidadEnExistencia;
+    }
+
     // Constructor, getters y setters
 }
---
+++ Venta.java
@@ -0,0 +1,33 @@
+public class Venta{
+    private int dia;
+    private int mes;
+    private int ano;
+    private double totalVendido;
+    
+    public int getDia() {
+        return dia;
+    }
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+    public int getMes() {
+        return mes;
+    }
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+    public int getAno() {
+        return ano;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public double getTotalVendido() {
+        return totalVendido;
+    }
+    public void setTotalVendido(double totalVendido) {
+        this.totalVendido = totalVendido;
+    }
+
+    // Constructor, getters y setters
+}
