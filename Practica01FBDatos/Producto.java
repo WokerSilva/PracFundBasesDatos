1abfb17caa802cd6dd5effce8fc50e0c33ade4f0
wokersilva <marcosilva@ciencias.unam.mx>
2023-09-03 14:43:58 -0600

    Agrupando en carpeta

---
+++ Practica01FBDatos/Producto.java
@@ -0,0 +1,40 @@
+public class Producto{
+    private int id;
+    private String nombre;
+    private String categoria;
+    private double precio;
+    private int cantidadEnExistencia;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public int getCantidadEnExistencia() {
+        return cantidadEnExistencia;
+    }
+    public void setCantidadEnExistencia(int cantidadEnExistencia) {
+        this.cantidadEnExistencia = cantidadEnExistencia;
+    }
+
+    // Constructor, getters y setters
+}
