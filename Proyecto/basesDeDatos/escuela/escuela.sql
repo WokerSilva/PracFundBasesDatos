5b9d2b8ef01eb1748263d9d6d822af6952510cc3
wokersilva <marcosilva@ciencias.unam.mx>
2023-12-07 01:19:08 -0600

    Primera version de la base de datos

---
+++ Proyecto/basesDeDatos/escuela/escuela.sql
@@ -0,0 +1,54 @@
+CREATE TABLE UBICACION
+(
+  ubicacionID VARCHAR(50) NOT NULL,
+  Localidad VARCHAR(255) NOT NULL,
+  Pais CHAR(100) NOT NULL,
+  PRIMARY KEY (ubicacionID)
+);
+
+CREATE TABLE MAESTROS
+(
+  maestroID VARCHAR(20) NOT NULL,
+  Nombre CHAR(50) NOT NULL,
+  Apellido CHAR(50) NOT NULL,
+  Especialidad CHAR(50) NOT NULL,
+  fechaNacimiento DATE NOT NULL,
+  Género VARCHAR(20) NOT NULL,
+  ubicacionID VARCHAR(50) NOT NULL,
+  PRIMARY KEY (maestroID),
+  FOREIGN KEY (ubicacionID) REFERENCES UBICACION(ubicacionID)
+);
+
+CREATE TABLE CURSOS
+(
+  cursoID VARCHAR(20) NOT NULL,
+  nombreCurso CHAR(50) NOT NULL,
+  gradoAcademico VARCHAR(20) NOT NULL,
+  maestroID VARCHAR(20) NOT NULL,
+  PRIMARY KEY (cursoID),
+  FOREIGN KEY (maestroID) REFERENCES MAESTROS(maestroID)
+);
+
+CREATE TABLE ASIGNATURAS
+(
+  asignaturaID VARCHAR(20) NOT NULL,
+  nombreAsignatura CHAR(30) NOT NULL,
+  maestroID VARCHAR(20) NOT NULL,
+  cursoID VARCHAR(20) NOT NULL,
+  FOREIGN KEY (cursoID) REFERENCES CURSOS(cursoID)
+);
+
+CREATE TABLE ALUMNOS
+(
+  alumnoID VARCHAR(20) NOT NULL,
+  Nombre CHAR(50) NOT NULL,
+  Apellido CHAR(50) NOT NULL,
+  Casa CHAR(20) NOT NULL,
+  fechaNacimiento DATE NOT NULL,
+  Género VARCHAR(20) NOT NULL,
+  ubicacionID VARCHAR(50) NOT NULL,
+  cursoID VARCHAR(20) NOT NULL,
+  PRIMARY KEY (alumnoID),
+  FOREIGN KEY (ubicacionID) REFERENCES UBICACION(ubicacionID),
+  FOREIGN KEY (cursoID) REFERENCES CURSOS(cursoID)
+);
